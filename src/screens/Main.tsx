f0d9861c8366068ad6564d368b3d778fa0b8ece3
AsmaElkady <asmaa.melkady@gmail.com>
2022-06-27 16:10:19 +0200

    update(main): add naviagtion and update interface

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,29 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { View, FlatList, ListRenderItem } from 'react-native';
 import NewsHeading from '../components/News/NewsHeading';
-import axios, { AxiosResponse } from 'axios';
-
-interface News {
-    source: Source,
-    title: string,
-    description: string,
-    url: string,
-    urlToImage: string,
-    publishedAt: string,
-    content: string
-}
-
-export interface Source {
-    id: any,
-    name: string
-}
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp, } from "@react-navigation/stack";
+import { NewsStackParamList } from '../navigation';
+import { INews } from '../interfaces'
 
 const Main: React.FC = () => {
-    const [news, setNews] = useState<News[]>([])
+    const [news, setNews] = useState<INews[]>([])
     const [page, setPage] = useState<number>(1)
     const [count, setCount] = useState<number>(0)
     const [totalItems, setTotalItems] = useState<number>(0)
     const [loading, setLoading] = useState<boolean>(true)
+    const navigation = useNavigation<StackNavigationProp<NewsStackParamList, "Main">>();
 
     useEffect(() => {
         if (totalItems >= count) {
@@ -33,20 +23,23 @@ const Main: React.FC = () => {
 
     const getData: () => void = () => {
         setLoading(true)
-        axios.get<News[]>('https://newsapi.org/v2/top-headlines?country=us&apiKey=6172c905911f4f77af1edb2f0df4d25c&pageSize=10&page=' + page)
+        axios.get<INews[]>('https://newsapi.org/v2/top-headlines?country=us&apiKey=6172c905911f4f77af1edb2f0df4d25c&pageSize=10&page=' + page)
             .then(async (response: AxiosResponse) => {
+                console.log(response)
                 setLoading(false)
                 if (response.status == 200) {
                     news.length === 0 ? await setNews(response.data.articles) : await news.push(...response.data.articles)
                     await setTotalItems(response.data.totalResults)
                     await setCount(news.length)
-                }
+                } 
+            }).catch((error: AxiosError)=>{
+                console.log('error', error)
             })
     }
 
-    const renderItem: ListRenderItem<News> = ({ item, index }) => {
+    const renderItem: ListRenderItem<INews> = ({ item, index }) => {
         return (
-            <NewsHeading key={index} title={item.title} img={item.urlToImage} onPress={() => console.log(item)} />
+            <NewsHeading key={index} title={item.title} img={item.urlToImage} onPress={() => navigation.navigate('NewsDetails', { details: item })} />
         )
     }
 
